a440fb528bce2d671ae9efd52b3dd608833fd231
2eo2yeo <2eo2yeo@gmail.com>
2026-01-28 01:13:56 +0900

    26.01.28 헤더/푸터

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-        <footer className="border-t">
-            <div className="mx-auto w-full max-w-screen-xl px-4 md:px-6 lg:px-8">
-                <p>© {new Date().getFullYear()} YUNA & 2EO All rights reserved.</p>
+        <footer className="border-t border-t-[#DDDDDD] py-3">
+            <div className="mx-auto w-full max-w-screen-xl px-2 md:px-6 lg:px-8">
+                <p className="text-center text-xs font-bold">© {new Date().getFullYear()} YUNA & 2EO All rights reserved.</p>
             </div>
         </footer>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,38 @@
 export default function Header() {
-    return (
-        <header className="border-b">
-            <div className="mx-auto w-full max-w-screen-xl px-4 md:px-6 lg:px-8"> 
-                <strong className="text-lg">Moviefy</strong>
+  return (
+    <header className="shadow-sm">
+      <div className="mx-auto max-w-screen-xl px-4 py-5 md:px-6 lg:px-8">
+        <div className="flex items-center">
+          <a href="/">
+            <img src="/assets/mf_logo.svg" alt="무비파이 로고" />
+          </a>
 
-            <nav>
-            </nav>
-            </div>
-        </header>
-    );
+          {/* lg+: input */}
+          <div className="relative mx-auto hidden lg:block">
+            <input
+              className="h-10 w-[320px] rounded-md border border-gray-300 pl-5 pr-10 focus:outline-none focus:ring-2 focus:ring-primary/40"
+              type="text"
+              placeholder="검색"
+            />
+            <img
+              src="/assets/search_icon.svg"
+              alt=""
+              className="pointer-events-none absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2"
+            />
+          </div>
+
+          {/* mobile: icons */}
+          <div className="ml-auto flex items-center lg:gap-3">
+            <button type="button" className="inline-flex h-10 w-10 items-center justify-center lg:hidden" aria-label="검색">
+              <img src="/assets/search_icon.svg" alt="검색 아이콘" className="h-5 w-5" />
+            </button>
+
+            <a href="https://github.com/Yuna-Kim98/moviefy" className="inline-flex h-10 w-10 items-center justify-center">
+              <img src="/assets/github_logo.svg" alt="깃허브 로고" className="h-6 w-6" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
